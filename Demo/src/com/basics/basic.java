935cc6b8158f69a1b89e094a0ec494454a16ce5a
techserv <techserv@192.168.0.19>
2024-04-19 16:34:04 -0500

    Sample commit

--- Demo/src/com/basics/basic.java
+++ Demo/src/com/basics/basic.java
@@ -4,10 +4,10 @@ public class basic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+	
 		System.out.println("hello world123");
 		System.out.println("Hello 2345");
-		
+		System.out.println("New world");
 
 	}
 
